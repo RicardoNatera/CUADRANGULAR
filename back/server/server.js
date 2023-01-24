772c164e5b3ed9c53b126e56b5608f875163d860
Ricardo Natera <ricardoantonionaterasubero@gmail.com>
2023-01-24 11:12:24 -0400

    Cambio de puerto en local

--- back/server/server.js
+++ back/server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const generalRouter = require('./src/routes/general.route')
 const userRouter = require('./src/routes/user.route')
 
