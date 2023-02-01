8cc900ca4bdc6d880f3880eae7b1857f9083de76
Ricardo Natera <ricardoantonionaterasubero@gmail.com>
2023-02-01 10:48:57 -0400

    color nombre de grupos

--- front/webpage/src/components/GroupItem.jsx
+++ front/webpage/src/components/GroupItem.jsx
@@ -5,13 +5,13 @@ import { FaTrashAlt } from 'react-icons/fa'
 function GroupItem({grupo}) {
     const dispatch = useDispatch()
   return (
-    <div className="grupo" style={{color:grupo.color}}>
+    <div className="grupo">
         <div>
             {
             // new Date(grupo.createdAt).toLocalString('en-US')
             }
         </div>
-        <h2>{grupo.nombre}</h2>
+        <h2 style={{color:grupo.color}}>{grupo.nombre}</h2>
         <h3>De {grupo.edadInicio} a {grupo.edadFinal} años</h3>
         <button onClick={()=>dispatch(deleteGroup(grupo.id_grupo))} className="close">
             <FaTrashAlt color="red"/>
