352b83189434fa81356cb60d7982a3d64e45a47c
Ricardo Natera <ricardoantonionaterasubero@gmail.com>
2023-02-16 12:48:06 -0400

    creacion de codigos qr y descarga de tarjetas

---
+++ front/webpage/src/components/Card.jsx
@@ -0,0 +1,27 @@
+
+import { useEffect, useRef, useState } from 'react';
+var QRCode = require('qrcode')
+
+function Card({codigo,grupo,maestro,domEl,color}) {
+    const [text, setText] = useState(`Grupo: ${grupo}\nMaestro: ${maestro}`);
+    const canvasRef = useRef();
+
+    useEffect(() => {
+        QRCode.toCanvas(
+          canvasRef.current,
+          text || " ",
+          (error) => error && console.error(error)
+        );
+    }, [text]);
+
+  return (
+    <div id="domEl" ref={domEl} className="tarjeta hidden" style={{borderColor:color, borderStyle:'solid',borderWidth:'10px'}}>
+        <section>
+            <canvas ref={canvasRef} />
+            <h3>{codigo}</h3>
+        </section>
+    </div>
+  )
+}
+
+export default Card
--- front/webpage/src/components/CardItem.jsx
+++ front/webpage/src/components/CardItem.jsx
@@ -1,21 +1,36 @@
+import { useRef } from 'react';
 import { useDispatch } from "react-redux"
 import { deleteCard } from '../features/tarjetas/tarjetasSlice'
-import { FaTrashAlt } from 'react-icons/fa'
+import { FaTrashAlt, FaDownload } from 'react-icons/fa'
+import * as htmlToImage from 'html-to-image';
+import Card from './Card'
+
 
 function CardItem({ card, grupo, maestro }) {
   const dispatch = useDispatch()
+  const domEl = useRef(null)
+
+  const downloadCard=async () =>{
+    domEl.current.className="tarjeta visible" //Elemento se vuelve visible
+    const dataUrl = await htmlToImage.toPng(domEl.current);
+
+    const link = document.createElement('a');
+    link.download = 'Tarjeta.png';
+    link.href = dataUrl;
+    link.click();  
+    domEl.current.className="tarjeta hidden" //Elemento se esconde
+  }
   return (
     <div className="grupo">
-        <div>
-            {
-            // new Date(grupo.createdAt).toLocalString('en-US')
-            }
-        </div>
+      <Card codigo={card.codigo} grupo={grupo ? grupo.nombre:""} maestro={maestro ? maestro.nombre+' '+maestro.apellido:""} color={grupo ? grupo.color:""} domEl={domEl}/>
         <h3>{card.codigo}</h3>
         {grupo ? <h4>Grupo: {grupo.nombre}</h4>:<></>}
         {maestro ? <h4>Maestro: {maestro.nombre} {maestro.apellido}</h4>:<></>}
         <button onClick={()=>dispatch(deleteCard(card.codigo))} className="close">
             <FaTrashAlt color="red"/>
+        </button>
+        <button onClick={()=>downloadCard()} className="download">
+            <FaDownload color="green"/>
         </button>
     </div>
   )
