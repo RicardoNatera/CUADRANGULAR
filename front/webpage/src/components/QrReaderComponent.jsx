95f5ab893e452b501f6399ab925abd61fd20a4a9
Ricardo Natera <ricardoantonionaterasubero@gmail.com>
2023-02-16 14:51:27 -0400

    mejora en home
    no mostrar scanner si no esta la camara disponible

--- front/webpage/src/components/QrReaderComponent.jsx
+++ front/webpage/src/components/QrReaderComponent.jsx
@@ -4,16 +4,19 @@ import {QrScanner} from '@yudiel/react-qr-scanner';
 
 function QrReaderComponent() {
     const [webcamResult, setWebcamResult] = useState();
-  const webcamError = (error) => {
+    const [error,setError] = useState(false)
+    const webcamError = (error) => {
     if (error) {
       setWebcamResult(error?.message)
+      setError(true)
       if(error?.message==="Requested device not found"){
-        setWebcamResult("No hay camara web")
+        setWebcamResult("No hay camara web disponible")
     }
     }
   };
   const webcamScan = (result) => {
     if (result) {
+      setError(false)
       setWebcamResult(result);
     }
     
@@ -24,12 +27,17 @@ const previewStyle = {
   }
     return (
         <>
-            <QrScanner
+          {
+            !error ? (
+              <QrScanner
                 onDecode={(result) => webcamScan(result)}
                 onError={(error) => webcamError(error)}
-            />
-            <p>WebCam Result: {webcamResult}</p>
-          </>
+              />
+            ):(<></>)
+          }
+          
+          <p>Resultado del lector: {webcamResult}</p>
+        </>
     )
 }
 
