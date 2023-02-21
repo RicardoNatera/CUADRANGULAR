0179bafad998812e7a62c7fdde1d87dfd35b4144
Ricardo Natera <ricardoantonionaterasubero@gmail.com>
2023-02-21 16:04:32 -0400

    header responsive

--- front/webpage/src/components/Header.jsx
+++ front/webpage/src/components/Header.jsx
@@ -1,65 +1,105 @@
-import { FaSignInAlt, FaSignOutAlt, FaUser, FaSchool } from 'react-icons/fa'
+import { useState } from 'react'
+import { FaSignInAlt, FaSignOutAlt, FaUser, FaSchool, FaBars } from 'react-icons/fa'
 import {Link, useNavigate, useLocation  } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { logout, reset } from '../features/auth/authSlice'
 import logo  from '../img/logo.png'
 
+import {
+    MDBNavbar,
+    MDBContainer,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBNavbarToggler,
+    MDBNavbarBrand,
+    MDBCollapse,
+    MDBBtn
+} from 'mdb-react-ui-kit';
+
 function Header() {
+    const [showNav, setShowNav] = useState(false);
+
     const location = useLocation()
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    const {user} = useSelector((state) => state.auth)
+
+    const { user } = useSelector((state) => state.auth)
+
     const onLogout = () =>{
         dispatch(logout())
         dispatch(reset())
         navigate('/')
     }
+
     const onDashboard = () =>{
         navigate('/dashboard')
     }
+
   return (
-    <header className='header'>
-        <div className="logo">
-            <Link to='/'><img src={logo} alt="logo"/></Link>
-        </div>
-        <ul>
-            {user && location.pathname=="/" ? 
+    <header className='mb-5'>
+        <MDBNavbar expand='lg' light bgColor='light'>
+        <MDBContainer fluid>
+          <MDBNavbarBrand >
+                <div className="logo">
+                    <Link to='/'><img src={logo} alt="logo"/></Link>
+                </div>
+            </MDBNavbarBrand>
+
+            <MDBNavbarToggler
+            type='button'
+            data-target='#navbar'
+            aria-controls='navbar'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+            onClick={() => setShowNav(!showNav)}
+          >
+            <FaBars/>
+          </MDBNavbarToggler>
+
+          <MDBCollapse show={showNav} navbar>
+            <MDBNavbarNav right fullWidth={false} className='mb-2 mb-lg-0'>
+              
+             { user && location.pathname=="/" ? 
             (
                 <>
-                    <li>
-                        <button className='btn' onClick={onDashboard}>
+                    <MDBNavbarItem>
+                        <MDBBtn size="md" className="mx-1" color='light' rippleColor='dark' onClick={onDashboard}>
                             <FaSchool/> Dashboard
-                        </button>
-                    </li>
-                    <li>
-                        <button className='btn' onClick={onLogout}>
+                        </MDBBtn>
+                    </MDBNavbarItem>
+                    <MDBNavbarItem>
+                        <MDBBtn size="md" className="mx-1" color='light' rippleColor='dark' onClick={onLogout}>
                             <FaSignOutAlt/> Logout
-                        </button>
-                    </li>
+                        </MDBBtn>
+                    </MDBNavbarItem>
                 </>
             ) : user ? (
-                <li>
-                    <button className='btn' onClick={onLogout}>
+                <MDBNavbarItem>
+                    <MDBBtn size="md" className="mx-1" color='light' rippleColor='dark' onClick={onLogout}>
                         <FaSignOutAlt/> Logout
-                    </button>
-                </li>
+                    </MDBBtn>
+                </MDBNavbarItem>
             ):
             (
                 <> 
-                    <li>
-                    <Link to='/login'>
-                        <FaSignInAlt/> Login
-                    </Link>
-                    </li>
-                    <li>
-                        <Link to='/register'>
+                    <MDBNavbarItem>
+                        <MDBNavbarLink  href='/login'>
+                            <FaSignInAlt/> Login
+                        </MDBNavbarLink>
+                    </MDBNavbarItem>
+                    <MDBNavbarItem>
+                        <MDBNavbarLink  href='/register'>
                             <FaUser/> Register
-                        </Link>
-                    </li>
+                        </MDBNavbarLink>
+                    </MDBNavbarItem>
                 </>
             )}
+              </MDBNavbarNav>
             
-        </ul>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
     </header>
   )
 }
