5b36d08be423acbe9897ad2f3c473a41eef70b05
Ricardo Natera <ricardoantonionaterasubero@gmail.com>
2023-03-05 18:28:51 -0500

    conect to planetScale DB

--- back/server/src/services/db.service.js
+++ back/server/src/services/db.service.js
@@ -1,9 +1,7 @@
 const mysql = require('mysql2/promise');
-var env = process.env.NODE_ENV || 'development';
-var dbConfig = require('../configs/db.config')[env];
 
 async function query(sql, params) {
-  const connection = await mysql.createConnection(dbConfig);
+  const connection = await mysql.createConnection(process.env.DATABASE_URL);
   const [results, ] = await connection.execute(sql, params);
 
   connection.end();
